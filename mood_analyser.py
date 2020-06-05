e8f24d72f5176fb8ac4fecc0567af9775763f0af
250399 <kunaldeshmukh2503@gmail.com>
2020-06-05 13:59:05 -0700

    REFACTORED: code to take parameters through constructor

--- mood_analyser.py
+++ mood_analyser.py
@@ -1,9 +1,20 @@
-
+from multipledispatch  import dispatch
 
 class mood_analyser:
+    message=""
 
+    def __init__(self,*message):
+        if(len(message)>0):
+            mood_analyser.message=message[0]
+            self.analyse_mood()
 
+    @dispatch(str)
     def analyse_mood(self,message):
-        if "happy" in message.lower():
+        mood_analyser.message=message
+        return self.analyse_mood()
+
+    @dispatch()
+    def analyse_mood(self):
+        if "happy" in mood_analyser.message.lower():
             return "Happy"
         return "Sad"
