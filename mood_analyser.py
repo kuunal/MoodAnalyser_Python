2fd770cf14fae614b2eb38b4b58af723e5bf598a
250399 <kunaldeshmukh2503@gmail.com>
2020-06-05 01:06:20 -0700

    ADDED: code for analyzing happy message

--- mood_analyser.py
+++ mood_analyser.py
@@ -4,6 +4,6 @@ class mood_analyser:
 
 
     def analyse_mood(self,message):
-        if "happy" in message:
+        if "happy" in message.lower():
             return "Happy"
         return "Sad"
